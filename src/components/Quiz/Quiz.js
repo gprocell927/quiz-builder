914f0d6bf2c6dbffbdcdfa35b4699ea0ab794966
Gabi Procell <gprocell927@gmail.com>
2017-02-18 19:02:10 -0700

    wip: rendering quiz questions to page

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 class Quiz extends Component {
+  constructor(){
+    super()
+    this.state = {
+      quiz: []
+    }
+  }
 
-  componentDidMount(){
+
+  componentWillMount(){
       axios
-        .get('http://localhost:3001/quizzes')
-        .then(res => console.log(res))
+        .get('http://localhost:3001/quizzes/')
+        .then(res => this.setState({ quiz: res.data.quizzes[0].questions }))
   }
 
   render(){
+    console.log(this.state.quiz[0])
+    const displayQuestions = () => {
+      this.state.quiz.map((q, i) => {
+        return (
+          <div>{q[i].title}</div>
+        )
+      })
+    }
     return(
-    <div>Testing</div>
+    <div>
+      <div>testing</div>
+      {this.state.quiz ? this.displayQuestions() : <h3>No quiz</h3>}
+    </div>
     )
   }
 }
